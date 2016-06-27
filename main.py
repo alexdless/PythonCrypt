e457cacbee9792c9009bb72d902c17b9e069e10c
bilka00 <bilka00@mail.ru>
2016-06-27 17:18:51 +0300

    Main module

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def main(args):
 		sys.exit()
     print "File Info: "
     print pe.FILE_HEADER
+    print "---"
+
+    print "[*] PE Section Information:"
+    for section in pe.sections:
+	    print "\t[+] Name: %s, Virtual Address: %s, Virtual Size: %s, Characteristics: %s" % (section.Name,
+																		 hex(section.VirtualAddress),
+																		 hex(section.Misc_VirtualSize),
+																		 hex(section.Characteristics))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
